48bf0a91667ff5346d3cd595dfa2d4ede09c5bf2
MazenTayseer <maziiin360@yahoo.com>
2024-05-17 00:21:31 +0300

    deploy: getting ready for phase 4 deploy

---
+++ app/operations/page.tsx
@@ -0,0 +1,192 @@
+"use client";
+
+import AddImage from "@components/AddImage";
+import { useRef, useState } from "react";
+import axios from "axios";
+import "@styles/loader.css";
+
+interface ImageData {
+  image: File;
+  operation: string;
+}
+
+interface ProcessedImages {
+  image: File;
+  operation: string;
+}
+
+const NODES = ["master001", "node001"];
+
+const BasicPage = () => {
+  const [imageCount, setImageCount] = useState(1);
+  const [imagesData, setImagesData] = useState<ImageData[]>([]);
+  const [processedImages, setProcessedImages] = useState<ProcessedImages[]>([]);
+  const [machineLogs, setMachineLogs] = useState([]);
+
+  const addImageRef = useRef<HTMLDivElement>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const animateLoaderOut = () => {
+    const loader = document.querySelector(".loader-container");
+    loader?.classList.remove("animate-in");
+    loader?.classList.add("animate-out");
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 320);
+  };
+
+  const addImage = () => {
+    setImageCount((prevCount) => prevCount + 1);
+    if (addImageRef.current) {
+      setTimeout(() => {
+        addImageRef.current!.scrollIntoView({ behavior: "smooth" });
+      }, 0);
+    }
+  };
+
+  const handleSubmit = async () => {
+    setIsLoading(true);
+    try {
+      const formData = new FormData();
+
+      imagesData.forEach(({ image, operation }) => {
+        formData.append("images", image);
+        formData.append("operations", operation);
+      });
+
+      const response = await axios.post(
+        "http://40.71.40.201/upload",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data", // Set content type to multipart/form-data
+          },
+        }
+      );
+
+      console.log("Response:", response.data);
+      const finalImages = response.data.images.map(
+        async (imageUrl: string, index: number) => {
+          // Fetch each image
+          const url = `http://40.71.40.201/results/${imageUrl}`;
+          const imageResponse = await axios.get(url, {
+            responseType: "blob", // Ensure binary response
+          });
+
+          const originalImageName = imageUrl;
+          const imageFile = new File([imageResponse.data], originalImageName, {
+            type: "image/png",
+          });
+
+          return { image: imageFile, operation: "N/A" };
+        }
+      );
+
+      const processedImages = await Promise.all(finalImages);
+
+      setProcessedImages(processedImages);
+
+      // Do logs here
+      const machineLogs = response.data.machine_logs;
+      setMachineLogs(machineLogs);
+
+      // processedImages.forEach(async (processedImage) => {
+      //   await axios.delete(
+      //     `http://40.71.40.201/delete/${processedImage.image.name}`
+      //   );
+      // });
+
+      NODES.forEach(async (node) => {
+        await axios.delete(
+          `http://40.71.40.201/delete_files_from_nodes/${node}`
+        );
+      });
+
+      animateLoaderOut();
+      // setImagesData([]);
+      // setProcessedImages([]);
+    } catch (error) {
+      animateLoaderOut();
+      console.error("Error uploading images:", error);
+    }
+  };
+
+  const handleImageUpload = (data: ImageData, index: number) => {
+    setImagesData((prevImagesData) => {
+      const existingIndex = prevImagesData.findIndex(
+        (imageData, idx) => idx === index
+      );
+      if (existingIndex !== -1) {
+        const updatedImagesData = [...prevImagesData];
+        updatedImagesData[existingIndex] = data;
+        return updatedImagesData;
+      } else {
+        return [...prevImagesData, data];
+      }
+    });
+  };
+
+  return (
+    <>
+      {isLoading && (
+        <div className='loader-container animate-in'>
+          <div className='loader'></div>
+        </div>
+      )}
+
+      <section className='site-padding py-20'>
+        <div className='container flex gap-4'>
+          <div className=' w-9/12'>
+            <h2 className='text-4xl font-bold'>Basic Image Processing</h2>
+
+            <div onSubmit={handleSubmit}>
+              {[...Array(imageCount)].map((_, index) => (
+                <AddImage
+                  key={index}
+                  index={index}
+                  onImageUpload={handleImageUpload}
+                  processedImages={processedImages}
+                />
+              ))}
+              <div ref={addImageRef}></div>
+
+              <div className='flex justify-between items-center gap-6 mt-6'>
+                <button
+                  type='button'
+                  onClick={addImage}
+                  className='text-black font-semibold bg-white rounded-lg shadow-lg p-3 w-full'
+                >
+                  Add Image
+                </button>
+                <button
+                  onClick={handleSubmit}
+                  type='button'
+                  className='text-white font-semibold bg-black rounded-lg shadow-lg p-3 w-full'
+                >
+                  Upload
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className='w-3/12'>
+            <h2 className='text-4xl font-bold'>Logs</h2>
+            <div className='bg-white p-4 rounded-lg shadow-lg mt-6'>
+              {machineLogs.length > 0 ? (
+                <ul>
+                  {machineLogs.map((log) => (
+                    <li className=' text-green-500 font-semibold'>{log}</li>
+                  ))}
+                </ul>
+              ) : (
+                <p>Logs will appear here</p>
+              )}
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default BasicPage;
---
+++ components/AddImageAdvanced.tsx
@@ -0,0 +1,125 @@
+import "@styles/image-input.css";
+import { motion } from "framer-motion";
+import { useState, ChangeEvent, useEffect } from "react";
+
+interface ProcessedImages {
+  image: File;
+  operation: string;
+}
+
+interface AddImageProps {
+  onImageUpload: (
+    data: { image: File; operation: string },
+    index: number
+  ) => void;
+  processedImages: ProcessedImages[];
+  predictions: Record<string, []>;
+  index: number;
+}
+
+const AddImageAdvanced = ({
+  onImageUpload,
+  processedImages,
+  predictions,
+  index,
+}: AddImageProps) => {
+  const [imageFile, setImageFile] = useState<File | null>(null);
+  const [selectedOperation, setSelectedOperation] =
+    useState<string>("classification");
+
+  const [ImageBeforeSrc, setImageBeforeSrc] = useState<string>(
+    "/assets/placeholder.png"
+  );
+  const [ImageAfterSrc, setImageAfterSrc] = useState<string>(
+    "/assets/placeholder.png"
+  );
+
+  useEffect(() => {
+    if (imageFile && predictions.hasOwnProperty(imageFile.name)) {
+      processedImages.forEach((processedImage) => {
+        if (processedImage.image.name === imageFile.name) {
+          setImageAfterSrc(URL.createObjectURL(processedImage.image));
+        }
+      });
+
+      const imagePredictions = predictions[imageFile.name];
+      const predsUl = document.querySelector(".predictions");
+
+      while (predsUl?.firstChild) {
+        predsUl.removeChild(predsUl.firstChild);
+      }
+
+      imagePredictions.forEach((prediction, index) => {
+        const li = document.createElement("li");
+        const confidence = parseFloat(prediction[1]);
+        const confidencePercentage = (confidence * 100).toFixed(0);
+
+        li.innerHTML = `${index + 1}. ${
+          prediction[0]
+        } - ${confidencePercentage}%`;
+        predsUl?.appendChild(li);
+      });
+    }
+  }, [imageFile, processedImages || predictions]);
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      setImageFile(file);
+      setImageBeforeSrc(URL.createObjectURL(file));
+      onImageUpload({ image: file, operation: selectedOperation }, index);
+    }
+  };
+
+  const handleOperationChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedOperation(event.target.value);
+    if (imageFile != null) {
+      onImageUpload({ image: imageFile, operation: event.target.value }, index);
+    }
+  };
+
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: -100 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: -100 }}
+      transition={{ duration: 0.5, ease: "easeOut" }}
+      className='flex justify-center items-center gap-12 mt-6 bg-white p-5 rounded-xl shadow-lg'
+    >
+      <div className='flex flex-col w-full gap-4 max-w-sm'>
+        <input
+          type='file'
+          accept='image/*'
+          required
+          onChange={handleFileChange}
+        />
+
+        <select
+          className='bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+          value={selectedOperation}
+          onChange={handleOperationChange}
+        >
+          <option value='classification'>Classification</option>
+        </select>
+      </div>
+
+      <label className='text-center text-xl font-semibold w-full'>
+        Before
+        <img
+          src={ImageBeforeSrc}
+          alt='image'
+          className='rounded-lg shadow-lg object-cover w-96 img-before'
+        />
+      </label>
+
+      <div className='mt-6 w-full self-start bg-gray-100 p-3 rounded-lg'>
+        <p className='font-semibold text-2xl'>Our Top 3 Predictions</p>
+        <ul className='predictions italic text-lg'>
+          <li>See Predictions Here</li>
+        </ul>
+      </div>
+    </motion.div>
+  );
+};
+
+export default AddImageAdvanced;
